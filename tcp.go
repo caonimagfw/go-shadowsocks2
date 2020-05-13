426635a0c8f4651529a79fffea261f4ddb3e6978
caonimagfw <51797166+caonimagfw@users.noreply.github.com>
2020-05-13 17:33:01 +0800

    Update tcp.go

--- tcp.go
+++ tcp.go
@@ -123,7 +123,7 @@ func tcpRemote(addr string, redir string, shadow func(net.Conn) net.Conn) {
 					dUrl = redir;
 					defer c.Close()
 					c.(*net.TCPConn).SetKeepAlive(true)
-					c, err := net.Dial("tcp", redir)
+					c, err = net.Dial("tcp", redir)
 					logf("log c dial error %v", err)
 				}else{
 					return

ac13cd57e37c26b7f0f47a681b9916df062f4612
caonimagfw <51797166+caonimagfw@users.noreply.github.com>
2020-05-18 02:21:45 +0800

    Update tcp.go

--- tcp.go
+++ tcp.go
@@ -339,7 +339,7 @@ func serverTCP(l net.Listener, redir string, shadow func(net.Conn) net.Conn) {
 			//	return
 			//}
 			//c, ok := mm.Conn
-			m1, err = c.(*cmux.MuxConn)
+			m1, err := c.(*cmux.MuxConn)
 			if err != nil{
 				logf("Change cmux to conn failed")
 				return
